856e670a38e6f801b21a0948f5206d436c980159
barkatali111 <114578340+barkatali111@users.noreply.github.com>
2025-10-28 11:46:57 +0500

    Update 01-setup-system.sh

--- Panel/01-setup-system.sh
+++ Panel/01-setup-system.sh
@@ -1,1 +1,9 @@
-#
+#!/bin/bash
+# Step 1: System Update & Dependencies
+
+set -e
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y curl zip unzip git software-properties-common lsb-release ca-certificates gnupg ufw
+
+echo "✅ System updated and basic packages installed."
