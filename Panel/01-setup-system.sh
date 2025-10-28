f395810e0fb909ae255fc16a67979e2fca529eba
barkatali111 <114578340+barkatali111@users.noreply.github.com>
2025-10-28 11:56:30 +0500

    01-setup-system.sh

--- Panel/01-setup-system.sh
+++ Panel/01-setup-system.sh
@@ -7,3 +7,4 @@ sudo apt upgrade -y
 sudo apt install -y curl zip unzip git software-properties-common lsb-release ca-certificates gnupg ufw
 
 echo "✅ System updated and basic packages installed."
+
